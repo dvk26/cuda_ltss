36c5172b9e6a2225e83596039326da984b9a98c5
Van Ky <kydang26092004@gmail.com>
2025-12-04 23:15:08 +0700

    update code gpu

--- src_v2/gpu_net.cu
+++ src_v2/gpu_net.cu
@@ -1,46 +1,7 @@
-#include <cuda_runtime.h>
+#include "gpu_net.hpp"
 #include <random>
+#include <vector>
 #include "gpu_kernels.cuh"
-
-struct GPUNet {
-    // ===== Weights, bias, grads =====
-    float *w1, *b1, *gw1, *gb1;
-    float *w2, *b2, *gw2, *gb2;
-    float *w3, *b3, *gw3, *gb3;
-    float *w4, *b4, *gw4, *gb4;
-    float *w5, *b5, *gw5, *gb5;
-
-    // ===== Activations =====
-    float *x;               // [N,3,32,32]
-    float *a1, *a1_relu, *p1;
-    float *a2, *a2_relu, *p2;
-    float *a3, *a3_relu, *u1;
-    float *a4, *a4_relu, *u2;
-    float *out;
-
-    // ===== Gradients =====
-    float *g_x;
-    float *g_a1, *g_a1_relu, *g_p1;
-    float *g_a2, *g_a2_relu, *g_p2;
-    float *g_a3, *g_a3_relu, *g_u1;
-    float *g_a4, *g_a4_relu, *g_u2;
-    float *g_out;   // sẽ dùng mse_dPred
-
-    // ===== Pool indices =====
-    int *idx_p1, *idx_p2;
-
-    // ===== MSE buffers =====
-    float *mse_buf, *mse_dPred, *mse_loss_dev;
-
-    int Nmax;
-
-    GPUNet(int Nmax_);
-    ~GPUNet();
-
-    void forward(int N);
-    float loss(const float* d_target, int N);
-    void backward(float lr, int N);
-};
 
 GPUNet::GPUNet(int Nmax_) : Nmax(Nmax_) {
     // 1) cudaMalloc weights/bias + gradients
