387d1ba51b35d49833a016b0d2ae395b472e17b8
Van Ky <kydang26092004@gmail.com>
2025-12-16 20:55:28 +0700

    update to train svm by cuml

--- src/train_svm_cuml/train_svm.py
+++ src/train_svm_cuml/train_svm.py
@@ -48,3 +48,16 @@ model.fit(X_train_scaled, y_train)
 # Đánh giá
 acc = model.score(X_test_scaled, y_test)
 print(f"Test Accuracy: {acc * 100:.2f}%")
+
+import pickle
+import os
+
+os.makedirs('out', exist_ok=True)
+
+with open('out/svm_model.pkl', 'wb') as f:
+    pickle.dump(model, f)
+
+with open('out/scaler.pkl', 'wb') as f:
+    pickle.dump(scaler, f)
+
+print("Model & scaler saved.")
